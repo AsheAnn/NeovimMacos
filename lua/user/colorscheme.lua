6aab603433d1568962cf8f3d14a38112982e9c08
ashe <asiasheann@gmail.com>
2022-03-16 12:20:18 +0800

    update plugin

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,30 @@
+----customtokyonight
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_lualine_bold = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+vim.g.tokyonight_colors = {error = "#ff001e", green = "#49ff86"}
+
+
+----dracula
+vim.g.dracula_transparent_bg = true
+vim.g.dracula_colors = {yellow = "#ffeb19",green = "#49ff86"}
+
+vim.g.NeoSolarized_transparent_bg = true
+
+
+vim.cmd [[
+try
+  
+""colorscheme NeoSolarized 
+  colorscheme tokyonight 
+""colorscheme onedarkpro
+""colorscheme onedark
+""colorscheme dracula
+""colorscheme ayu  
+
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
---
+++ lua/user/stabilize.lua
@@ -0,0 +1,17 @@
+local status_ok, stabilize = pcall(require, "stabilize")
+if not status_ok then
+  return
+end
+
+
+stabilize.setup {
+  default = {
+    force = true, -- stabilize window even when current cursor position will be hidden behind new window
+	  forcemark = '', -- set context mark to register on force event which can be jumped to with '<forcemark>
+	  ignore = {  -- do not manage windows matching these file/buftypes
+		  filetype = { "help", "list", "Trouble" },
+		  buftype = { "terminal", "quickfix", "loclist" },
+	},
+	nested = '' -- comma-separated list of autocmds that wil trigger the plugins window restore function
+  }
+}
