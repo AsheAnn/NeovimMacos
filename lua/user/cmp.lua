cbc9bb0df7c7473f1af46a0ee460d7c94679e4e8
ashe <asiasheann@gmail.com>
2022-04-19 02:37:22 +0800

    edit cmp.lua

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -89,13 +89,13 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       -- NOTE: order matters
       vim_item.menu = ({
+        luasnip = "[Snippet]",
+        cmp_tabnine ="[TN]",
         nvim_lsp = "[LSP]",
         nvim_lua = "[Nvim]",
-        luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
         emoji = "[Emoji]",
-        cmp_tabnine ="[TN]",
 
         -- nvim_lsp = "",
         -- nvim_lua = "",
@@ -108,13 +108,13 @@ cmp.setup {
     end,
   },
   sources = {
+    { name = "luasnip" },
+    { name = "cmp_tabnine" },
     { name = "nvim_lsp" },
     { name = "nvim_lua" },
-    { name = "luasnip" },
     { name = "buffer" },
-    { name = "cmp_tabnine" },
     { name = "path" },
-    -- { name = "emoji" },
+    { name = "emoji" },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
